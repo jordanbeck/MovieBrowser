7a339d85176f4ae9a4b8105a6a8bf40a8b0257a3
jordanbeck <jordan.s.beck@gmail.com>
2016-11-16 17:27:54 -0600

    Adding more information to the details screen.

--- app/src/main/java/com/twentyfivesquares/moviebrowser/controller/DetailController.java
+++ app/src/main/java/com/twentyfivesquares/moviebrowser/controller/DetailController.java
@@ -1,20 +1,31 @@
 package com.twentyfivesquares.moviebrowser.controller;
 
 import android.content.Context;
+import android.text.TextUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
 import com.twentyfivesquares.moviebrowser.R;
 import com.twentyfivesquares.moviebrowser.model.Movie;
 
 public class DetailController extends TinyController {
 
+    private ImageView vPoster;
     private TextView vTitle;
+    private TextView vSummary;
 
     public DetailController(Context context, Movie movie) {
         super(context);
 
+        vPoster = (ImageView) findViewById(R.id.detail_poster);
+        Picasso.with(context).load(movie.poster).into(vPoster);
+
         vTitle = (TextView) findViewById(R.id.detail_title);
         vTitle.setText(movie.title);
+
+        vSummary = (TextView) findViewById(R.id.detail_summary);
+        vSummary.setText(TextUtils.isEmpty(movie.plot) ? context.getString(R.string.msg_no_summary) : movie.plot);
     }
 
     @Override
