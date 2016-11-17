526755b1c4773e66ac6e137382864860ba2c493f
jordanbeck <jordan.s.beck@gmail.com>
2016-11-16 20:53:39 -0600

    Adding the search box to the top of the search page.

--- app/src/main/java/com/twentyfivesquares/moviebrowser/MainActivity.java
+++ app/src/main/java/com/twentyfivesquares/moviebrowser/MainActivity.java
@@ -1,8 +1,8 @@
 package com.twentyfivesquares.moviebrowser;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.twentyfivesquares.moviebrowser.adapter.MovieAdapter;
--- app/src/main/java/com/twentyfivesquares/moviebrowser/controller/SearchController.java
+++ app/src/main/java/com/twentyfivesquares/moviebrowser/controller/SearchController.java
@@ -1,10 +1,14 @@
 package com.twentyfivesquares.moviebrowser.controller;
 
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
 
 import com.twentyfivesquares.moviebrowser.R;
 import com.twentyfivesquares.moviebrowser.adapter.MovieAdapter;
@@ -20,10 +24,19 @@ import retrofit.client.Response;
 public class SearchController extends TinyController {
 
     private RecyclerView vList;
+    private TextView vEmpty;
     private MovieAdapter adapter;
 
     public SearchController(Context context, MovieAdapter.OnMovieSelectedListener movieSelectedListener) {
         super(context);
+
+        // This is a hack to keep the keyboard hidden when the app launches
+        getView().post(new Runnable() {
+            @Override
+            public void run() {
+                hideKeyboard(getContext());
+            }
+        });
 
         adapter = new MovieAdapter();
         adapter.setOnMovieSelectedListener(movieSelectedListener);
@@ -33,10 +46,13 @@ public class SearchController extends TinyController {
         vList.setLayoutManager(layoutManager);
         vList.setAdapter(adapter);
 
+        vEmpty = (TextView) findViewById(R.id.search_empty);
+
         MovieApi api = new MovieApi();
         api.search("Star Wars", new Callback<List<Movie>>() {
             @Override
             public void success(List<Movie> movies, Response response) {
+                showList();
                 adapter.update(movies);
             }
 
@@ -50,5 +66,27 @@ public class SearchController extends TinyController {
     @Override
     protected int getLayoutRes() {
         return R.layout.controller_search;
+    }
+
+    private void hideKeyboard(Context context) {
+        View view = ((Activity) context).getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    private void showEmpty() {
+        if (vEmpty.getVisibility() != View.VISIBLE) {
+            vList.setVisibility(View.GONE);
+            vEmpty.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void showList() {
+        if (vList.getVisibility() != View.VISIBLE) {
+            vEmpty.setVisibility(View.GONE);
+            vList.setVisibility(View.VISIBLE);
+        }
     }
 }
