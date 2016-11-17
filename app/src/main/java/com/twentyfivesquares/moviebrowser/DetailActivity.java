7a2c9a286bd90413091f2e2ea1aac5b433f52c50
jordanbeck <jordan.s.beck@gmail.com>
2016-11-16 19:32:23 -0600

    Converting to toolbar.

--- app/src/main/java/com/twentyfivesquares/moviebrowser/DetailActivity.java
+++ app/src/main/java/com/twentyfivesquares/moviebrowser/DetailActivity.java
@@ -3,6 +3,8 @@ package com.twentyfivesquares.moviebrowser;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.twentyfivesquares.moviebrowser.controller.DetailController;
 
@@ -14,8 +16,25 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Build the controller and set the view
         final String id = getIntent().getStringExtra(EXTRA_MOVIE_ID);
         DetailController controller = new DetailController(this, id);
         setContentView(controller.getView());
+
+        // Initialize the toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(R.string.title_detail);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/com/twentyfivesquares/moviebrowser/MainActivity.java
+++ app/src/main/java/com/twentyfivesquares/moviebrowser/MainActivity.java
@@ -3,6 +3,7 @@ package com.twentyfivesquares.moviebrowser;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.twentyfivesquares.moviebrowser.adapter.MovieAdapter;
 import com.twentyfivesquares.moviebrowser.controller.MainController;
@@ -14,8 +15,15 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Build the controller and set the view
         final MainController controller = new MainController(this, this);
         setContentView(controller.getView());
+
+        // Initialize the toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.app_name);
     }
 
     @Override
